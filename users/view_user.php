471f741c2862bfbe2bce8e452fd552223b670552
mwelk11 <mwelk11@gmail.com>
2014-04-03 20:01:49 -0400

    View User Working
    View user is working. Displays all necessary information

--- users/view_user.php
+++ users/view_user.php
@@ -12,42 +12,33 @@ require '../includes/html.header.inc.php';
 // Format messages for display
 $messages = formatMessages();
 
-$query = "SELECT * FROM people WHERE first_name = 'Matt'";
-$result = mysql_query($query);
-$row = 0;
+$sql = "SELECT * FROM people WHERE seller_id = :seller_id";
+$params = array(':seller_id' => $_SESSION['user']['id']);
+$result = query($sql,$params);
+$row = fetch($result);
 
-$password = mysql_result($result, $row, "password");
-$firstName = mysql_result($result, $row, "first_name");
-$lastName = mysql_result($result, $row, "last_name");
-$email = mysql_result($result, $row, "email");
-$phoneNumber = mysql_result($result, $row, "phone_number");
-$age = mysql_result($result, $row, "age");
-$gender = mysql_result($result, $row, "gender");
-$income = mysql_result($result, $row, "annual_income");
+$sql = "SELECT username FROM sellers WHERE seller_id = :seller_id";
+$params = array('seller_id' => $_SESSION['user']['id']);
+$result = query($sql,$params);
+$row2 = fetch($result);
 
 ?>
 <?php echo (isset($messages)) ? $messages : '';?>
 
 <div class="section_content">
 	<body>
-		<h1>Username:</h1><br>
-			<p><?php echo $username; ?></p><br>
-		<h1>Password:</h1><br>
-			<p><?php echo $password; ?></p><br>
-		<h1>First Name:</h1><br>
-			<p><?php echo $first_name; ?></p><br>
-		<h1>Last Name:</h1><br>
-			<p><?php echo $last_name; ?></p><br>
-		<h1>Email:</h1><br>
-			<p><?php echo $email; ?></p><br>
-		<h1>Phone Number:</h1><br>
-			<p><?php echo $phone_number; ?></p><br>
-		<h1>Age:</h1><br>
-			<p><?php echo $age; ?></p><br>
-		<h1>Gender:</h1><br>
-			<p><?php echo $gender; ?></p><br>
-		<h1>Annual Income:<h1/><br>
-			<p><?php echo $annual_income; ?></p><br>
+		<h1><b>Username:</b>
+			<?php echo $row2['username']; ?></h1><br>
+		<h1><b>First Name:</b>
+			<?php echo $row['first_name']; ?></h1><br>
+		<h1><b>Last Name:</b>
+			<?php echo $row['last_name']; ?></h1><br>
+		<h1><b>Email:</b>
+			<?php echo $row['email']; ?></h1><br>
+		<h1><b>Phone Number:</b>
+			<?php echo $row['phone_number']; ?></h1><br>
+		<h1><b>Age:</b>
+			<?php echo $row['age']; ?></h1><br>
 	</body>
 </div>
 
