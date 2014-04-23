b14b3389a1829a14c2c7c1d8261b3006db042986
kevinkoolwhip <kevin.koleck@gmail.com>
2014-04-23 17:16:38 -0400

    Welcome Page updated

--- index.php
+++ index.php
@@ -31,8 +31,27 @@ if(isset($request['action']))
 }
 
 require_once 'includes/html.header.inc.php';
+?>
 
+<?php echo (isset($messages)) ? $messages : '';?>
 
+<!--<div class="section_description">Please Complete the Search Form Below.</div> -->
+<div class="section_content">
+	<form id="person_search_form" class="input_text" method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+	<div class="section_title">Welcome To E - Auction</div>
+	<div class="section_title_divider"></div>	
+	E-Auction is an online auction site where you can sell and buy items from other users. 
+	<br/>
+	<br/>
+	We are currently in our beta stage and working hard improving E-auction and its functionality
+	<br/>
+	<br/>
+	Happy Bidding!
+	
+	</form>
+</div>
+
+<?php
 require 'includes/footer.inc.php';
 
 ?>	
